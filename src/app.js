9a52632ae1630b692241ab60c57d5dbdad63b873
gamesfriends <gamesfriendsun@gmail.com>
2024-06-07 10:57:00 -0300

    EJS
    Implementation of EJS, and that the
    route returns the first view of the
    product page. The morgan module was
    implemented to display detailed
    information about the requests
    
    Resolve: T#003

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import express from "express"
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+import morgan from "morgan";
 
 import productsRouter from "./routes/products.routes.js";
 
 const app = express();
 
+// Define the system path.
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Settings
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 // Middlewares.
 app.use(express.json())
+app.use(morgan('tiny'))
 
 // Routes.
 app.use("/products", productsRouter);
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,7 +3,10 @@ import { Router } from "express"
 const productsRouter = Router()
 
 productsRouter.get('/', (_req, res) => {
-  res.send("Products")
+  res.render('index', {
+    tabTitle: 'First Website with EJS.', 
+    mainTitle: 'Mi primer sitio web con EJS como motor de plantillas.'
+  })
 })
 
 export default productsRouter;
