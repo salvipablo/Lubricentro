0ffaa6d998f30491bfc71e31ee2c7cb317eeff7e
gamesfriends <gamesfriendsun@gmail.com>
2024-06-11 09:13:15 -0300

    Refactor BBDD
    Refactoring of the code for connecting
    the database and data in environment
    variables
    
    Resolve: R#001

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
-import { config } from "dotenv";
+import { config } from "dotenv"
 
 config()
 
-export const PORT = process.env.PORT;
-export const DB_DATABASE = process.env.DB_DATABASE;
-export const DB_DIALECT = process.env.DB_DIALECT;
+export const PORT = process.env.PORT
+export const DB_DATABASE = process.env.DB_DATABASE
+export const DB_DIALECT = process.env.DB_DIALECT
+export const PATH_APP = process.env.PATH_APP
--- src/database/database.js
+++ src/database/database.js
@@ -3,14 +3,12 @@ import { Sequelize } from "sequelize"
 import { 
   DB_DATABASE,
   DB_DIALECT,
+  PATH_APP
 } from '../config.js'
+
+console.log(PATH_APP);
 
 export const sequelize = new Sequelize(DB_DATABASE, "", "", {
   dialect: DB_DIALECT,
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-  storage: "C:/Users/Pablo/Documents/Programming/Projects/Lubricentro/src/database/lubricentro.sqlite"
+  storage: `${PATH_APP}/database/lubricentro.sqlite`
 })
